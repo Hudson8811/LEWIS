806892fa0bf9ab29758336eb25d7a7df07fcd648
741963Hudson <hudson88@yandex.ru>
2021-04-28 10:00:16 +0300

    change

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/* libs */
+
+//= libs/jquery.min.js
+//= libs/jquery.pagepiling.min.js
+//= libs/jquery.pagepiling.min.js
+
+//= libs/gsap.min.js
+
+/* Partials */
+
+//= partials/header.js
+//= partials/pagepiling.js
+
---
+++ src/js/partials/pagepiling.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    // $('#pagepiling').pagepiling({
+    //     menu: null,
+    //     direction: 'vertical',
+    //     verticalCentered: true,
+    //     sectionsColor: [],
+    //     anchors: [],
+    //     scrollingSpeed: 400,
+    //     easing: 'swing',
+    //     loopBottom: false,
+    //     loopTop: false,
+    //     css3: true,
+    //     navigation: {
+    //         'textColor': '#000',
+    //         'bulletsColor': '#000',
+    //         'position': 'right',
+    //         'tooltips': ['section1', 'section2', 'section3', 'section4']
+    //     },
+    //     normalScrollElements: null,
+    //     normalScrollElementTouchThreshold: 5,
+    //     touchSensitivity: 5,
+    //     keyboardScrolling: true,
+    //     sectionSelector: '.section',
+    //     animateAnchor: true,
+
+    //     //events
+    //     onLeave: function (index, nextIndex, direction) { },
+    //     afterLoad: function (anchorLink, index) { },
+    //     afterRender: function () { },
+    // });
+
+    $('#pagepiling').pagepiling({
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'red'],
+        verticalCentered: true,
+    });
+});
+
