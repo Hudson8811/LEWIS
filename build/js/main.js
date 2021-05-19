519520797ebbe3ee51eed1f6ddf3aed79b1ce227
hudson88 <hudson88@yandex.ru>
2021-05-19 14:38:52 +0300

    change

--- build/js/main.js
+++ build/js/main.js
@@ -1089,32 +1089,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuExit = document.querySelector('.header__menu-exit');
     const main = document.querySelector('.main');
     const header = document.querySelector('.header');
-    const social = document.querySelector('.header__menu-social')
-    const contacts = document.querySelector('.header__menu-contacts')
-    const list = document.querySelector('.header__menu-list');
-
-    function setAnimation(item, value, duration) {
-        return setTimeout(() => { item.style.opacity = value }, duration)
-    }
-
-    let timeout = null;
-    let timeout2 = null;
-    let timeout3 = null;
 
     burger.addEventListener('click', function () {
         menu.classList.add('active');
-        timeout = setAnimation(social, 1, 300)
-        timeout3 = setAnimation(list, 1, 400)
-        timeout2 = setAnimation(contacts, 1, 500)
     })
 
     menuExit.addEventListener('click', function () {
-        clearTimeout(timeout)
-        clearTimeout(timeout2)
-        clearTimeout(timeout3)
-        setAnimation(social, 0, 100)
-        setAnimation(contacts, 0, 100)
-        setAnimation(list, 0, 100)
         menu.classList.remove('active');
     })
 
